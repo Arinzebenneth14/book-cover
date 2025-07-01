aa55b31cdc2fa0e679a27c62b71d31c42ee6ac76
arinze <arinzebenneth@gmail.com>
2025-07-01 03:01:13 -0700

    Initial commit

---
+++ week_1/assignment.py
@@ -0,0 +1,47 @@
+full_name = "Chuka_Emeka"
+update_full_name = "Chuka_Okeke"
+print(update_full_name)
+
+height = 1.75
+age = 27
+print  (F"my height {height} my age {age}")
+
+is_logged_in = True
+print (is_logged_in)
+
+first_name = Ada
+last_name = Okoro
+
+concat = first_name +" "+ last_name
+print(concat)
+
+num1 = 29
+num2 = 5
+
+calcu = num1 / num2
+print(calcu)
+
+num1 = 1.94
+num2 = 1.84
+
+calcu = num1 + num2
+print(calcu)
+
+num4 = 7
+num4 = 5
+
+compare = 7 >=5
+print(compare)
+
+has_id = True
+has_clerance = True
+
+print(F"has_id and has clearance")
+
+has_pass = False
+knows_manager = True
+
+print(f"has_pass or knows_manager")
+
+can_enter = False
+print(f"cannot enter")
---
+++ week_1/day_2.py
@@ -0,0 +1,107 @@
+print("hello world")
+
+# variable and data types
+
+# data type 
+# integers
+12.3 #floats 
+"data in here" # string #
+True  # -boolean
+False  #- boolean
+#variables with data types
+full_name = "kingsley Okonkwo"
+age = 32 
+height = 1.95
+is_available= True
+
+#print(full_name, age, height, is_available)
+
+#print("Before update,full_name")
+
+#full_name = "kelechi iheanacho"
+
+#print("After update",full_name)
+
+PascalValues = ""
+
+# Operators - Arithmetic - Logical Operators
+
+# + - * / //(floor division) %(Modulus)
+
+num1 = 1
+num2 = 5
+
+add = num1 + num2 
+print(add)
+
+first_name="Kelechi"
+last_name= "Okonkwo"
+
+concat = first_name + " " + last_name # concatenation
+#print (concat)
+# f-string Literal
+
+with_string_literal = f"This {first_name} {last_name}" # interpolation
+print(with_string_literal)
+
+# Inbuilt features= keywords, funtions, class, methods
+# with in for def if else elif or while try except import class from finally return and
+
+# Inbuilt funtions - print() input()
+
+# Inbuilt classes - type(), for conversion: int() float() str() bool()
+
+a_string = input("what don you want to write:")
+# print ("output: ",a_string)
+
+print(
+    type(with_string_literal)
+)
+
+# Add numbers
+first_number = input ("Give me the first whole number:")
+second_number = input("Give me the second number:")
+print("Adding...")
+addition = Float(fisrt_number) + float(second_number)
+
+print(f"the total sum is (addition).")
+
+
+# comparisons -
+'''
+== equals to
+> greater than
+< less than 
+>= greater than an equals to
+!= not equals 
+'''
+
+num3 = 3
+num4 = 4
+
+compare = num3 <= num4
+# compare = "Telma" =="telma"
+print (compare)
+
+# Logical opertors - or and is not
+
+val = 1
+and_val = 1
+
+compare1 = (4 == 4) and (3 != 2) and ("Telma" == "Telma") # all of them must be true for it to be true
+compare2 = (2 > 4) or (9 < 6) # just needs one true comparison to be true
+
+
+bool = 4 > 2 
+compare4 = not bool #looks for what is false 
+print(compare3)
+
+int()
+float()
+str()
+bool()
+
+print(
+    type(is_available)
+)
+
